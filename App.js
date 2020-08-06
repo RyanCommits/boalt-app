7b92acb2d86ca1851ededa1921b91656feea48b0
Ryan Wang <qwang1993@gmail.com>
2020-08-06 11:16:07 -0400

    - add feature casting - add svg logo

--- App.js
+++ App.js
@@ -7,67 +7,66 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
+import Svg, {Use, Image, SvgUri, SvgXml} from 'react-native-svg';
+
+import BoaltLogo from './shared/BoaltLogoWhite.svg';
 import GoogleCast, {CastButton} from 'react-native-google-cast';
 
 const App = () => {
+  GoogleCast.EventEmitter.addListener(GoogleCast.SESSION_STARTED, () => {
+    GoogleCast.castMedia({
+      mediaUrl:
+        'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4',
+      imageUrl:
+        'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/images/480x270/BigBuckBunny.jpg',
+      title: 'Big Buck Bunny',
+      subtitle:
+        'A large and lovable rabbit deals with three tiny bullies, led by a flying squirrel, who are determined to squelch his happiness.',
+      studio: 'Blender Foundation',
+      streamDuration: 596, // seconds
+      contentType: 'video/mp4', // Optional, default is "video/mp4"
+      playPosition: 10, // seconds
+      customData: {
+        // Optional, your custom object that will be passed to as customData to reciever
+        customKey: 'customValue',
+      },
+    });
+  });
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <CastButton style={{width: 24, height: 24}} />
-          </View>
-        </ScrollView>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.header}>
+          <SvgXml width="200" height="100" xml={BoaltLogo} />
+        </View>
+        <View style={styles.content}>
+          <CastButton style={styles.castButton} />
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  header: {
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, .7)',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  content: {
+    backgroundColor: 'red',
+    flexGrow: 1,
+    alignItems: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  castButton: {
+    width: 60,
+    height: 60,
+    tintColor: 'white',
+    marginRight: -15,
   },
   sectionDescription: {
     marginTop: 8,
